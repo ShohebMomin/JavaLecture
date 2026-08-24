4244bee088a4bb2fb78d0a15c45a71960cd48261
Shoaib <mominshoaib4@gmail.com>
2026-08-24 14:32:43 +0530

    Increased the range of try block to monitor more lines of code

--- src/day36/ArithmeticExceptionDemo.java
+++ src/day36/ArithmeticExceptionDemo.java
@@ -1,20 +1,25 @@
 package day36;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ArithmeticExceptionDemo {
     static void main() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Numerator");
+        try{
         int numerator = sc.nextInt();
         System.out.println("Enter Denominator");
         int denominator = sc.nextInt();
-        try{
+
             double result = numerator/denominator;
             System.out.println(result);
         }
         catch (ArithmeticException ex){
             System.out.println(ex.getMessage());
+        }
+        catch (InputMismatchException ex){
+            System.out.println("Please enter valid integer values only");
         }
         System.out.println("Code continued");
     }
